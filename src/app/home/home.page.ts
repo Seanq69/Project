f77d002ff6ca2db0740aae4a9b72e0b258026308
unknown <g00420397@atu.ie>
2025-04-21 16:31:26 +0000

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { ServiceService } from '../services/service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  mySpace:any[] = [];
+  constructor(private ss:ServiceService) {}
+
+  ngOnInit(): void {
+    this.ss.getServiceData().subscribe(
+      (data)=>{
+        this.mySpace = data.Search;
+      }
+
+    );
+  }
 }
